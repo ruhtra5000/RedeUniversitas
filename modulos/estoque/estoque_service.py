66df8f832b3efd95ee1de7b91692b39645e1ff1b
Arthur de Sá Tenório <arthursa147@gmail.com>
2026-07-17 23:13:52 -0300

    feat: db + service da entidade Almoxarife

--- modulos/estoque/estoque_service.py
+++ modulos/estoque/estoque_service.py
@@ -61,3 +61,22 @@ def listarMovimentacaoId(idMovimentacao: int):
         raise Exception(f"Movimentação com id {idMovimentacao} não existente.")
     
     return movimentacao
+
+
+#   ___   _                                       _   __       
+#  / _ \ | |                                     (_) / _|      
+# / /_\ \| | _ __ ___    ___  __  __  __ _  _ __  _ | |_   ___ 
+# |  _  || || '_ ` _ \  / _ \ \ \/ / / _` || '__|| ||  _| / _ \
+# | | | || || | | | | || (_) | >  < | (_| || |   | || |  |  __/
+# \_| |_/|_||_| |_| |_| \___/ /_/\_\ \__,_||_|   |_||_|   \___|
+
+def listarAlmoxarifes():
+    return dbListarAlmoxarifes()
+    
+def listarAlmoxarifeId(idAlmoxarife: int):
+    almoxarife = dbListarAlmoxarifeId(idAlmoxarife)
+
+    if almoxarife == None:
+        raise Exception(f"Almoxarife com id {idAlmoxarife} não existente.")
+    
+    return almoxarife
