04257d7735e20d5a2ffa305760e857f3d446bcc5
Arthur de Sá Tenório <arthursa147@gmail.com>
2026-07-18 01:29:58 -0300

    feat: db + service de ContaPagar e ContaReceber

--- modulos/financeiro/financeiro_db.py
+++ modulos/financeiro/financeiro_db.py
@@ -1,3 +1,4 @@
+from datetime import date
 from decimal import Decimal
 
 from sqlalchemy import select
@@ -5,8 +6,10 @@ from sqlalchemy.exc import SQLAlchemyError
 
 from database.Conexao import SessionLocal
 from database.entidades.Caixa import Caixa
+from database.entidades.ContaPagar import ContaPagar
+from database.entidades.ContaReceber import ContaReceber
 from database.entidades.Financeiro import Financeiro
-import database.entidades 
+import database.entidades
 
 #  _____         _              
 # /  __ \       (_)             
@@ -50,11 +53,14 @@ def dbRemoverValorCaixa(idCaixa: int, valorDebito: Decimal):
             query = select(Caixa).where(Caixa.id == idCaixa)
             caixa = session.execute(query).scalar_one()
 
+            if (caixa.valor_caixa < valorDebito):
+                raise Exception(f"O valor a ser debitado excede o valor em caixa.")
+
             caixa.valor_caixa -= valorDebito
 
             session.commit()
 
-        except SQLAlchemyError:
+        except Exception:
             session.rollback()
             raise
 
@@ -79,3 +85,126 @@ def dbListarFinanceiroId(idFinanceiro: int):
         financeiros = session.execute(query).scalar_one_or_none()
 
         return financeiros
+    
+
+#  _____                _          ______                    _                 
+# /  __ \              | |         | ___ \                  | |                
+# | /  \/  ___   _ __  | |_   __ _ | |_/ /  ___   ___   ___ | |__    ___  _ __ 
+# | |     / _ \ | '_ \ | __| / _` ||    /  / _ \ / __| / _ \| '_ \  / _ \| '__|
+# | \__/\| (_) || | | || |_ | (_| || |\ \ |  __/| (__ |  __/| |_) ||  __/| |   
+#  \____/ \___/ |_| |_| \__| \__,_|\_| \_| \___| \___| \___||_.__/  \___||_|   
+
+def dbListarContasReceber():
+    with SessionLocal() as session:
+        query = select(ContaReceber)
+        contasReceber = session.execute(query).scalars().all()
+
+        return contasReceber
+
+# lista as contas a receber vencidas por campus (caixa é único por campus)
+def dbListarContasReceberCaixaVencidas(idCaixa: int):
+    hoje = date.today()
+
+    with SessionLocal() as session:
+        query = select(ContaReceber).where(ContaReceber.caixa_id == idCaixa, ContaReceber.data_vencimento < hoje)
+        contasReceber = session.execute(query).scalars().all()
+
+        return contasReceber
+    
+def dbListarContaReceberId(idContaReceber: int):
+    with SessionLocal() as session:
+        query = select(ContaReceber).where(ContaReceber.id == idContaReceber)
+        contaReceber = session.execute(query).scalar_one_or_none()
+
+        return contaReceber
+    
+def dbDefinirDataPagamentoContaReceber(idContaReceber: int, dataPagamento: date):
+    with SessionLocal() as session:
+        try:
+            query = select(ContaReceber).where(ContaReceber.id == idContaReceber)
+            contaReceber = session.execute(query).scalar_one()
+
+            if contaReceber.data_pagamento == None:
+                # Alterando data de pagamento
+                contaReceber.data_pagamento = dataPagamento
+
+                # Alterando mensalidade (sinalizar que foi pago)
+                mensalidade = contaReceber.mensalidade
+                mensalidade.foi_paga = True
+
+                # Adicionar valor no caixa
+                dbAdicionarValorCaixa(contaReceber.caixa_id, contaReceber.valor)
+
+                session.add(mensalidade)
+                session.add(contaReceber)
+                session.commit()
+            else:
+                raise Exception("A data de pagamento relativa à esta mensalidade já foi definida.")
+        
+        except Exception:
+            session.rollback()
+            raise
+
+def dbDefinirFinanceiroContaReceber(idContaReceber: int, idFinanceiro: int):
+    with SessionLocal() as session:
+        query = select(ContaReceber).where(ContaReceber.id == idContaReceber)
+        contaReceber = session.execute(query).scalar_one()
+
+        contaReceber.financeiro_id = idFinanceiro
+
+        session.commit()
+
+
+#  _____                _          ______                            
+# /  __ \              | |         | ___ \                           
+# | /  \/  ___   _ __  | |_   __ _ | |_/ / __ _   __ _   __ _  _ __ 
+# | |     / _ \ | '_ \ | __| / _` ||  __/ / _` | / _` | / _` || '__|
+# | \__/\| (_) || | | || |_ | (_| || |   | (_| || (_| || (_| || |   
+#  \____/ \___/ |_| |_| \__| \__,_|\_|    \__,_| \__, | \__,_||_|   
+#                                                 __/ |             
+#                                                |___/
+
+def dbListarContasPagar():
+    with SessionLocal() as session:
+        query = select(ContaPagar)
+        contasPagar = session.execute(query).scalars().all()
+
+        return contasPagar
+    
+# lista as contas a pagar vencidas por campus (caixa é único por campus)
+def dbListarContasPagarCaixaVencidas(idCaixa: int):
+    hoje = date.today()
+
+    with SessionLocal() as session:
+        query = select(ContaPagar).where(ContaPagar.caixa_id == idCaixa, ContaPagar.data_vencimento < hoje)
+        contasPagar = session.execute(query).scalars().all()
+
+        return contasPagar
+    
+def dbListarContaPagarId(idContaPagar: int):
+    with SessionLocal() as session:
+        query = select(ContaPagar).where(ContaPagar.id == idContaPagar)
+        contaPagar = session.execute(query).scalar_one_or_none()
+
+        return contaPagar
+
+def dbDefinirDataPagamentoContaPagar(idContaPagar: int, dataPagamento: date):
+    with SessionLocal() as session:
+        try:
+            query = select(ContaPagar).where(ContaPagar.id == idContaPagar)
+            contaPagar = session.execute(query).scalar_one()
+
+            if contaPagar.data_pagamento == None:
+                # Alterando data de pagamento
+                contaPagar.data_pagamento = dataPagamento
+
+                # Remover valor no caixa
+                dbRemoverValorCaixa(contaPagar.caixa_id, contaPagar.valor)
+
+                session.commit()
+            else:
+                raise Exception("A data de pagamento relativa à esta compra já foi definida.")
+        
+        except Exception:
+            session.rollback()
+            raise
--- modulos/financeiro/financeiro_service.py
+++ modulos/financeiro/financeiro_service.py
@@ -22,13 +22,7 @@ def adicionarValorCaixa(idCaixa: int, valorDeposito: Decimal):
     dbAdicionarValorCaixa(idCaixa, valorDeposito)
 
 def removerValorCaixa(idCaixa: int, valorDebito: Decimal):
-    caixa = listarCaixaId(idCaixa)
-
-    if (caixa.valor_caixa < valorDebito):
-        raise Exception(f"O valor a ser debitado excede o valor em caixa.")
-    else:
-        dbRemoverValorCaixa(idCaixa, valorDebito)
-
+    dbRemoverValorCaixa(idCaixa, valorDebito)
 
 # ______  _                                   _              
 # |  ___|(_)                                 (_)             
@@ -41,4 +35,66 @@ def listarFinanceiro():
     return dbListarFinanceiro()
     
 def listarFinanceiroId(idFinanceiro: int):
-    return dbListarFinanceiroId(idFinanceiro)
+    financeiro = dbListarFinanceiroId(idFinanceiro)
+
+    if financeiro == None:
+        raise Exception(f"Financeiro com id {idFinanceiro} não existente.")
+    
+    return financeiro
+
+
+#  _____                _          ______                    _                 
+# /  __ \              | |         | ___ \                  | |                
+# | /  \/  ___   _ __  | |_   __ _ | |_/ /  ___   ___   ___ | |__    ___  _ __ 
+# | |     / _ \ | '_ \ | __| / _` ||    /  / _ \ / __| / _ \| '_ \  / _ \| '__|
+# | \__/\| (_) || | | || |_ | (_| || |\ \ |  __/| (__ |  __/| |_) ||  __/| |   
+#  \____/ \___/ |_| |_| \__| \__,_|\_| \_| \___| \___| \___||_.__/  \___||_|   
+
+def listarContasReceber():
+    return dbListarContasReceber()
+
+# Lista as contas a receber vencidas por campus (caixa é único por campus)
+def listarContasReceberCaixaVencidas(idCaixa: int):
+    return dbListarContasReceberCaixaVencidas(idCaixa)
+    
+def listarContaReceberId(idContaReceber: int):
+    contaReceber = dbListarContaReceberId(idContaReceber)
+
+    if contaReceber == None:
+        raise Exception(f"ContaReceber com id {idContaReceber} não existente.")
+    
+    return contaReceber
+    
+def definirDataPagamentoContaReceber(idContaReceber: int, dataPagamento: date):
+    dbDefinirDataPagamentoContaReceber(idContaReceber, dataPagamento)
+
+def definirFinanceiroContaReceber(idContaReceber: int, idFinanceiro: int):
+    dbDefinirFinanceiroContaReceber(idContaReceber, idFinanceiro)
+
+
+#  _____                _          ______                            
+# /  __ \              | |         | ___ \                           
+# | /  \/  ___   _ __  | |_   __ _ | |_/ / __ _   __ _   __ _  _ __ 
+# | |     / _ \ | '_ \ | __| / _` ||  __/ / _` | / _` | / _` || '__|
+# | \__/\| (_) || | | || |_ | (_| || |   | (_| || (_| || (_| || |   
+#  \____/ \___/ |_| |_| \__| \__,_|\_|    \__,_| \__, | \__,_||_|   
+#                                                 __/ |             
+#                                                |___/
+
+def listarContasPagar():
+    return dbListarContasPagar()
+    
+# lista as contas a pagar vencidas por campus (caixa é único por campus)
+def listarContasPagarCaixaVencidas(idCaixa: int):
+    return dbListarContasPagarCaixaVencidas(idCaixa)
+    
+def listarContaPagarId(idContaPagar: int):
+    contaPagar = dbListarContaPagarId(idContaPagar)
+
+    if contaPagar == None:
+        raise Exception(f"ContaPagar com id {idContaPagar} não existente.")
+    
+    return contaPagar
+
+def definirDataPagamentoContaPagar(idContaPagar: int, dataPagamento: date):
+    dbDefinirDataPagamentoContaPagar(idContaPagar, dataPagamento)
