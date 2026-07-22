0a43c04df58096339945964e914b0ae7d4152c7a
Guilherme Paes <86077955+Guy1717@users.noreply.github.com>
2026-07-21 21:42:25 -0300

    feat: cria BASE para tela de cadastros

--- modulos/cadastros/cadastros.py
+++ modulos/cadastros/cadastros.py
@@ -1,6 +1,27 @@
 import streamlit as st
-
-# Tentativa falha de interface
+from modulos.rotas import ROTA_CADASTRO_ALUNO, ROTA_CADASTRO_PROFESSOR, ROTA_CADASTRO_CURSO, ROTA_CADASTRO_DISCIPLINA, ROTA_CADASTRO_TURMA
 
 def telaCadastros():
-    st.write("Cadastro")
+    st.title("Cadastros")
+
+    with st.container(horizontal=True):
+
+        if st.button("Alunos"):
+            st.session_state.pagina = ROTA_CADASTRO_ALUNO
+            st.rerun()
+
+        if st.button("Professores"):
+            st.session_state.pagina = ROTA_CADASTRO_PROFESSOR
+            st.rerun()
+
+        if st.button("Cursos"):
+            st.session_state.pagina = ROTA_CADASTRO_CURSO
+            st.rerun()
+
+        if st.button("Disciplinas"):
+            st.session_state.pagina = ROTA_CADASTRO_DISCIPLINA
+            st.rerun()
+
+        if st.button("Turmas"):
+            st.session_state.pagina = ROTA_CADASTRO_TURMA
+            st.rerun()
