94c78c9ae83f4afa792ca716496de394e40b530d
Guilherme Paes <86077955+Guy1717@users.noreply.github.com>
2026-07-27 16:22:01 -0300

    fix: corrige criacao de turma

--- modulos/cadastros/turma.py
+++ modulos/cadastros/turma.py
@@ -16,15 +16,14 @@ import database.entidades
 #       professor: Professor
 
 # Service
-def criarTurma(turma: Turma):
+def criarTurma(turma: Turma, curso, disciplina, professor):
     try:
-        turma.codigo = ""
         
-        if turma.curso_id != turma.disciplina.curso_id:
-            raise Exception(f"A disciplina selecionada deve pertencer ao curso {turma.curso.nome}.")
+        if curso.id != disciplina.curso_id:
+            raise Exception(f"A disciplina selecionada deve pertencer ao curso {curso.nome}.")
 
-        if turma.curso.campus_id != turma.professor.campus_id:
-            raise Exception(f"O professor designado para esta Turma deve pertencer ao Campus {turma.curso.campus.nome}.")
+        if curso.campus_id != professor.campus_id:
+            raise Exception(f"O professor designado para esta Turma deve pertencer ao Campus {curso.campus.nome}.")
         
         dbCriarTurma(turma)
             
