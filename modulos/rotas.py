8ad14ec3a574b8af5b69cad444b899f902a1b9e9
Guilherme Paes <86077955+Guy1717@users.noreply.github.com>
2026-07-27 22:31:14 -0300

    feat(ui/cadastros): atualiza sidebar

--- modulos/rotas.py
+++ modulos/rotas.py
@@ -20,20 +20,20 @@ home_page = st.Page(telaHome, title="Página Inicial", icon=":material/home:", d
 cadastros_page = st.Page(telaCadastros, title="Central de Cadastros", icon=":material/folder:", url_path="cadastros")
 
 # Subpáginas de Cadastro
-cadastro_aluno = st.Page(telaCadastroAluno, title="Aluno", icon=":material/person:", url_path="cadastro_aluno")
-cadastro_prof = st.Page(telaCadastroProfessor, title="Professor", icon=":material/school:", url_path="cadastro_professor")
-cadastro_curso = st.Page(telaCadastroCurso, title="Curso", icon=":material/auto_stories:", url_path="cadastro_curso")
-cadastro_disc = st.Page(telaCadastroDisciplina, title="Disciplina", icon=":material/menu_book:", url_path="cadastro_disciplina")
-cadastro_turma = st.Page(telaCadastroTurma, title="Turma", icon=":material/groups:", url_path="cadastro_turma")
+cadastro_aluno = st.Page(telaCadastroAluno, title="Aluno", icon=":material/person_add:", url_path="cadastro_aluno")
+cadastro_prof = st.Page(telaCadastroProfessor, title="Professor", icon=":material/group_add:", url_path="cadastro_professor")
+cadastro_curso = st.Page(telaCadastroCurso, title="Curso", icon=":material/library_add:", url_path="cadastro_curso")
+cadastro_disc = st.Page(telaCadastroDisciplina, title="Disciplina", icon=":material/post_add:", url_path="cadastro_disciplina")
+cadastro_turma = st.Page(telaCadastroTurma, title="Turma", icon=":material/group_add:", url_path="cadastro_turma")
 
 # Cadastros extras
-cadastro_almoxarife = st.Page(telaCadastroAlmoxarife, title="Almoxarife", icon=":material/inventory:", url_path="cadastro_almoxarife")
-cadastro_bolsa = st.Page(telaCadastroBolsa, title="Bolsa", icon=":material/card_membership:", url_path="cadastro_bolsa")
-cadastro_campus = st.Page(telaCadastroCampus, title="Campus", icon=":material/account_balance:", url_path="cadastro_campus")
-cadastro_compra = st.Page(telaCadastroCompra, title="Compra", icon=":material/shopping_cart:", url_path="cadastro_compra")
-cadastro_financeiro = st.Page(telaCadastroFinanceiro, title="Financeiro", icon=":material/payments:", url_path="cadastro_financeiro")
-cadastro_fornecedor = st.Page(telaCadastroFornecedor, title="Fornecedor", icon=":material/local_shipping:", url_path="cadastro_fornecedor")
-cadastro_matricula = st.Page(telaCadastroMatricula, title="Matrícula", icon=":material/assignment_ind:", url_path="cadastro_matricula")
+cadastro_almoxarife = st.Page(telaCadastroAlmoxarife, title="Almoxarife", icon=":material/person_add:", url_path="cadastro_almoxarife")
+cadastro_bolsa = st.Page(telaCadastroBolsa, title="Bolsa", icon=":material/loyalty:", url_path="cadastro_bolsa")
+cadastro_campus = st.Page(telaCadastroCampus, title="Campus", icon=":material/domain_add:", url_path="cadastro_campus")
+cadastro_compra = st.Page(telaCadastroCompra, title="Compra", icon=":material/add_shopping_cart:", url_path="cadastro_compra")
+cadastro_financeiro = st.Page(telaCadastroFinanceiro, title="Financeiro", icon=":material/person_add:", url_path="cadastro_financeiro")
+cadastro_fornecedor = st.Page(telaCadastroFornecedor, title="Fornecedor", icon=":material/person_add:", url_path="cadastro_fornecedor")
+cadastro_matricula = st.Page(telaCadastroMatricula, title="Matrícula", icon=":material/assignment_add:", url_path="cadastro_matricula")
 cadastro_estoque = st.Page(telaCadastroEstoque, title="Estoque", icon=":material/inventory_2:", url_path="cadastro_estoque")
 
 def get_navigation():
@@ -43,18 +43,19 @@ def get_navigation():
             home_page,
             cadastros_page,
         ],
-        "Formulários de Cadastro": [
+
+        "Cadastros": [
             cadastro_aluno,
             cadastro_prof,
+            cadastro_financeiro,
+            cadastro_fornecedor,
+            cadastro_almoxarife,
+            cadastro_campus,
             cadastro_curso,
             cadastro_disc,
             cadastro_turma,
-            cadastro_almoxarife,
             cadastro_bolsa,
-            cadastro_campus,
             cadastro_compra,
-            cadastro_financeiro,
-            cadastro_fornecedor,
             cadastro_matricula,
             cadastro_estoque
         ]
