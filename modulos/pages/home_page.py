7ca69d61d6837d1a16c7d122b830ff5f1b7d507b
PacssauroRex <victorcauaorg@gmail.com>
2026-08-10 12:34:52 -0300

    Aplicação do visual na página home

--- modulos/pages/home_page.py
+++ modulos/pages/home_page.py
@@ -1,9 +1,11 @@
 import streamlit as st
 
-def telaHome():
-    pessoa_nome = st.session_state.get("pessoa_logada", st.user.name)
+from modulos.utils.home_visual import renderizarHome
 
-    st.title("RedeUniversitas")
-    st.write(f"Bem-vindo(a) de volta, **{pessoa_nome}**!")
-    
-    st.caption("Selecione uma aba pelo painel lateral para utilizar o sistema")
+def telaHome():
+    pessoa_nome = st.session_state.get(
+        "pessoa_logada",
+        st.user.name,
+    )
+
+    renderizarHome(pessoa_nome) 
