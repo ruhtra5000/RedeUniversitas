7ec9ced9559b1b71dd10223da02f39e2e7234e5a
PacssauroRex <victorcauaorg@gmail.com>
2026-08-10 13:41:44 -0300

    Adição de notas com valor inicial 0 para resolução do bug de cadastro de mátricula

--- modulos/cadastros/matricula.py
+++ modulos/cadastros/matricula.py
@@ -1,11 +1,9 @@
 from sqlalchemy.exc import SQLAlchemyError
-
 from database.Conexao import SessionLocal
 from database.entidades.Aluno import Aluno
 from database.entidades.Disciplina import Disciplina
 from database.entidades.Matricula import Matricula
-import database.entidades
-
+from decimal import Decimal
 
 # === Dados recebidos ===
 # - matricula:
@@ -19,42 +17,67 @@ import database.entidades
 def criarMatricula(matricula: Matricula, aluno: Aluno, disciplina: Disciplina):
     try:
         if aluno.curso_id != disciplina.curso_id:
-            raise Exception(f"O aluno deve pertencer ao mesmo curso da disciplina.")
-        
+            raise Exception("O aluno deve pertencer ao mesmo curso da disciplina.")
+
         with SessionLocal() as session:
-            # Pegamos os pre requisitos da disciplina
             from sqlalchemy import select
             from database.entidades.PreRequisito import PreRequisito
             from database.entidades.Matricula import Matricula as MatrDB
-            
-            preReqs = session.execute(
-                select(PreRequisito).where(PreRequisito.disciplina_id == disciplina.id)
-            ).unique().scalars().all()
-            
-            matrAluno = session.execute(
-                select(MatrDB).where(MatrDB.aluno_id == aluno.pessoa_id)
-            ).unique().scalars().all()
-            
+
+            preReqs = (
+                session.execute(
+                    select(PreRequisito).where(
+                        PreRequisito.disciplina_id == disciplina.id
+                    )
+                )
+                .unique()
+                .scalars()
+                .all()
+            )
+
+            matrAluno = (
+                session.execute(
+                    select(MatrDB).where(MatrDB.aluno_id == aluno.pessoa_id)
+                )
+                .unique()
+                .scalars()
+                .all()
+            )
+
             flag = True
+
             for preReq in preReqs:
                 cursado = False
+
                 for matr in matrAluno:
                     if preReq.prerequisito_id == matr.disciplina_id:
                         cursado = True
-                        if matr.aprovacao == None or matr.aprovacao == False:
+
+                        if matr.aprovacao is None or matr.aprovacao is False:
                             flag = False
 
                 if not cursado or not flag:
-                    raise Exception(f"Algum dos pré-requisitos da disciplina não foi concluído ou cursado.")
-        
+                    raise Exception(
+                        "Algum dos pré-requisitos da disciplina "
+                        "não foi concluído ou cursado."
+                    )
+
+        # Valores iniciais da matrícula
+        matricula.nota1 = Decimal("-1.00")
+        matricula.nota2 = Decimal("-1.00")
+        matricula.nota3 = Decimal("-1.00")
+        matricula.final = Decimal("-1.00")
+        matricula.media = Decimal("0.00")
+        matricula.frequencia_abs = 0
+        matricula.frequencia_rel = 0.0
+
         dbCriarMatricula(matricula)
-            
-    except SQLAlchemyError:    
-        raise
-    
-    except Exception:
+
+    except SQLAlchemyError:
         raise
 
+    except Exception:
+        raise
 
 # Dados
 def dbCriarMatricula(matricula: Matricula):
