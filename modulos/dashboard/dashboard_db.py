421ca8e01f44aa5ab633466332dccf219fc8838b
PacssauroRex <victorcauaorg@gmail.com>
2026-08-16 00:39:14 -0300

    Adição de função para contar Alunos trancados / Adição de verificação para garantir que o valor não entre como NONE / Adição de verificação para garantir que não haja divisão por zero / Correção de Bugs

---
+++ modulos/dashboard/dashboard_db.py
@@ -0,0 +1,473 @@
+from datetime import date
+from sqlalchemy import case, func, or_, select
+from sqlalchemy.exc import SQLAlchemyError
+from database.Conexao import SessionLocal
+from database.entidades.Aluno import Aluno
+from database.entidades.Bolsa import Bolsa
+from database.entidades.ContaReceber import ContaReceber
+from database.entidades.Curso import Curso
+from database.entidades.Matricula import Matricula
+from database.entidades.Mensalidade import Mensalidade
+from database.entidades.Professor import Professor
+from database.entidades.enums.StatusAluno import StatusAluno
+import database.entidades
+from database.entidades.enums.StatusBolsa import StatusBolsa
+
+
+#  _____                     _ 
+# |  __ \                   | |
+# | |  \/  ___  _ __   __ _ | |
+# | | __  / _ \| '__| / _` || |
+# | |_\ \|  __/| |   | (_| || |
+#  \____/ \___||_|    \__,_||_|
+
+def dbContarAlunosAtivos(
+        idCampus: int | None = None,
+        idCurso: int | None = None
+    ):
+    with SessionLocal() as session:
+        query = select(func.count(Aluno.pessoa_id)).where(Aluno.status == StatusAluno.ATIVO)
+
+        if idCampus is not None:
+            query = query.where(Aluno.campus_id == idCampus)
+
+        elif idCurso is not None: 
+            query = query.where(Aluno.curso_id == idCurso)
+
+        qtdeAlunos = session.scalar(query)
+
+        return qtdeAlunos
+
+def dbContarAlunosFormados(
+        idCampus: int | None = None,
+        idCurso: int | None = None
+    ):
+    with SessionLocal() as session:
+        query = select(func.count(Aluno.pessoa_id)).where(Aluno.status == StatusAluno.FORMADO)
+
+        if idCampus is not None:
+            query = query.where(Aluno.campus_id == idCampus)
+
+        elif idCurso is not None: 
+            query = query.where(Aluno.curso_id == idCurso)
+
+        qtdeAlunos = session.scalar(query)
+
+        return qtdeAlunos
+
+def dbContarAlunosEvadidos(
+        idCampus: int | None = None,
+        idCurso: int | None = None
+    ):
+    with SessionLocal() as session:
+        query = select(func.count(Aluno.pessoa_id)).where(Aluno.status == StatusAluno.EVADIDO)
+
+        if idCampus is not None:
+            query = query.where(Aluno.campus_id == idCampus)
+
+        elif idCurso is not None: 
+            query = query.where(Aluno.curso_id == idCurso)
+
+        qtdeAlunos = session.scalar(query)
+
+        return qtdeAlunos
+    
+def dbContarAlunosTrancados(
+        idCampus: int | None = None,
+        idCurso: int | None = None
+    ):
+    with SessionLocal() as session:
+        query = select(func.count(Aluno.pessoa_id)).where(Aluno.status == StatusAluno.TRANCADO)
+
+        if idCampus is not None:
+            query = query.where(Aluno.campus_id == idCampus)
+
+        elif idCurso is not None: 
+            query = query.where(Aluno.curso_id == idCurso)
+
+        qtdeAlunos = session.scalar(query)
+
+        return qtdeAlunos
+
+def dbCalcularTaxaEvasao():
+    with SessionLocal() as session:
+        query = select(func.count(Aluno.pessoa_id))
+        qtdeTotalAlunos = session.scalar(query)
+
+        qtdeAlunosEvadidos = dbContarAlunosEvadidos()
+    
+        return (qtdeAlunosEvadidos / qtdeTotalAlunos)
+
+def dbContarProfessores(
+        idCampus: int | None = None,
+        idCurso: int | None = None
+    ):
+    with SessionLocal() as session:
+        query = select(func.count(Professor.pessoa_id))
+
+        if idCampus is not None:
+            query = query.where(Professor.campus_id == idCampus)
+
+        elif idCurso is not None: 
+            query = query.where(Professor.curso_id == idCurso)
+
+        qtdeProfessores = session.scalar(query)
+
+        return qtdeProfessores
+
+def dbContarCursos(
+        idCampus: int | None = None,
+    ):
+    with SessionLocal() as session:
+        query = select(func.count(Curso.id))
+
+        if idCampus is not None:
+            query = query.where(Curso.campus_id == idCampus)
+
+        qtdeCursos = session.scalar(query)
+
+        return qtdeCursos
+
+
+#   ___                    _                   _              
+#  / _ \                  | |                 (_)             
+# / /_\ \  ___   __ _   __| |  ___  _ __ ___   _   ___   ___  
+# |  _  | / __| / _` | / _` | / _ \| '_ ` _ \ | | / __| / _ \ 
+# | | | || (__ | (_| || (_| ||  __/| | | | | || || (__ | (_) |
+# \_| |_/ \___| \__,_| \__,_| \___||_| |_| |_||_| \___| \___/ 
+
+def dbCalcularCoefRendMedio(
+        idCampus: int | None = None,
+        idCurso: int | None = None
+    ):
+    with SessionLocal() as session:
+        query = select(func.avg(Aluno.coef_rend))
+    
+        if idCampus is not None:
+            query = query.where(Aluno.campus_id == idCampus)
+
+        elif idCurso is not None:
+            query = query.where(Aluno.curso_id == idCurso)
+            
+        crMedio = session.scalar(query)
+    
+        return crMedio
+
+# Retorna a quantidade de alunos em categorias de desempenho
+def dbAgruparAlunosDesempenho(
+        idCampus: int | None = None,
+        idCurso: int | None = None    
+    ):
+    categoria = case(
+        (Aluno.coef_rend >= 8.5, "Excelente"),
+        (Aluno.coef_rend >= 7.0, "Bom"),
+        (Aluno.coef_rend >= 5.5, "Regular"),
+        else_="Ruim"
+    )
+
+    with SessionLocal() as session:
+        query = (
+            select(
+                categoria.label("categoria"),
+                func.count(Aluno.pessoa_id).label("quantidade")
+            )
+            .group_by(categoria)
+        )
+
+        if idCampus is not None:
+            query = query.where(Aluno.campus_id == idCampus)
+        
+        elif idCurso is not None: 
+            query = query.where(Aluno.curso_id == idCurso)
+
+        resultado = session.execute(query).all()
+
+        return resultado
+
+# Baixo desempenho: CR < 5.5 ou 3+ reprovaçoes
+def dbAlunosBaixoDesempenho(
+        idCampus: int | None = None,
+        idCurso: int | None = None 
+    ):
+    with SessionLocal() as session:
+        reprovacoes = (
+            select(func.count(Matricula.turma_id))
+            .where(
+                Matricula.aluno_id == Aluno.pessoa_id,
+                Matricula.aprovacao == False
+            )
+            .scalar_subquery()
+        )
+
+        query = select(Aluno).where(
+            or_(Aluno.coef_rend < 5.5, reprovacoes >= 3)
+        )
+    
+        if idCampus is not None:
+            query = query.where(Aluno.campus_id == idCampus)
+            
+        elif idCurso is not None: 
+            query = query.where(Aluno.curso_id == idCurso)
+    
+        alunos = session.execute(query).scalars().all()
+    
+        return alunos
+
+
+# ______  _                                   _              
+# |  ___|(_)                                 (_)             
+# | |_    _  _ __    __ _  _ __    ___   ___  _  _ __   ___  
+# |  _|  | || '_ \  / _` || '_ \  / __| / _ \| || '__| / _ \ 
+# | |    | || | | || (_| || | | || (__ |  __/| || |   | (_) |
+# \_|    |_||_| |_| \__,_||_| |_| \___| \___||_||_|    \___/ 
+
+def dbCalcularReceita(
+        idCampus: int | None = None,
+        idCurso: int | None = None
+    ):
+    with SessionLocal() as session:
+        query = (
+            select(func.sum(ContaReceber.valor))
+            .join(ContaReceber.mensalidade)
+            .join(Mensalidade.aluno)
+            .where(ContaReceber.data_pagamento.is_not(None))
+        )
+        
+        if idCampus is not None:
+            query = query.where(Aluno.campus_id == idCampus)
+    
+        elif idCurso is not None:
+            query = query.where(Aluno.curso_id == idCurso)
+                
+        receita = select(func.coalesce(func.sum(ContaReceber.valor), 0))
+        
+        return receita
+
+def dbCalcularTotalAReceber(
+        idCampus: int | None = None,
+        idCurso: int | None = None
+    ):
+    with SessionLocal() as session:
+        query = (
+            select(func.sum(ContaReceber.valor))
+            .join(ContaReceber.mensalidade)
+            .join(Mensalidade.aluno)
+            .where(ContaReceber.data_pagamento.is_(None))
+        )
+            
+        if idCampus is not None:
+            query = query.where(Aluno.campus_id == idCampus)
+        
+        elif idCurso is not None:
+            query = query.where(Aluno.curso_id == idCurso)
+                    
+        receita = select(func.coalesce(func.sum(ContaReceber.valor), 0))
+            
+        return receita
+
+def dbContarAlunosInadimplentes(
+        idCampus: int | None = None,
+        idCurso: int | None = None
+    ):
+    hoje = date.today()
+
+    with SessionLocal() as session:
+        query = (
+            select(func.count(Aluno.pessoa_id.distinct()))
+            .join(Aluno.mensalidades)
+            .where(
+                Mensalidade.data_vencimento < hoje, 
+                Mensalidade.foi_paga == False
+            )
+        )
+
+        if idCampus is not None:
+            query = query.where(Aluno.campus_id == idCampus)
+        
+        elif idCurso is not None: 
+            query = query.where(Aluno.curso_id == idCurso)
+
+        qtdeAlunos = session.scalar(query)
+
+        return qtdeAlunos
+
+def dbCalcularTaxaInadimplencia(
+        idCampus: int | None = None,
+        idCurso: int | None = None
+    ):
+    with SessionLocal() as session:
+        query = select(func.count(Aluno.pessoa_id))
+
+        if idCampus is not None:
+            query = query.where(Aluno.campus_id == idCampus)
+            qtdeInad = dbContarAlunosInadimplentes(idCampus=idCampus)
+
+        elif idCurso is not None: 
+            query = query.where(Aluno.curso_id == idCurso)
+            qtdeInad = dbContarAlunosInadimplentes(idCurso=idCurso)
+            
+        else:
+            qtdeInad = dbContarAlunosInadimplentes()
+
+        qtdeAlunos = session.scalar(query) or 0
+        qtdeInad = qtdeInad or 0
+
+        if not qtdeAlunos:
+            return 0
+
+        return (qtdeInad / qtdeAlunos)
+
+def dbCalcularValorTotalInadimplente(
+        idCampus: int | None = None,
+        idCurso: int | None = None
+    ):
+    hoje = date.today()
+
+    with SessionLocal() as session:
+        query = (
+            select(func.coalesce(func.sum(Mensalidade.valor), 0))
+            .join(Mensalidade.aluno)
+            .where(
+                Mensalidade.data_vencimento < hoje, 
+                Mensalidade.foi_paga == False
+            )
+        )
+
+        if idCampus is not None:
+            query = query.where(Aluno.campus_id == idCampus)
+        
+        elif idCurso is not None: 
+            query = query.where(Aluno.curso_id == idCurso)
+
+        valorTotal = session.scalar(query)
+
+        return valorTotal
+
+def dbContarMensalidadesVencidas(
+        idCampus: int | None = None,
+        idCurso: int | None = None 
+    ):  
+    hoje = date.today()
+
+    with SessionLocal() as session:
+        query = (
+            select(func.count(Mensalidade.id))
+            .join(Mensalidade.aluno)
+            .where(
+                Mensalidade.data_vencimento < hoje, 
+                Mensalidade.foi_paga == False
+            )
+        )
+
+        if idCampus is not None:
+            query = query.where(Aluno.campus_id == idCampus)
+        
+        elif idCurso is not None: 
+            query = query.where(Aluno.curso_id == idCurso)
+
+        qtdeTotal = session.scalar(query)
+
+        return qtdeTotal
+
+def dbCalcularDividaMedia(
+        idCampus: int | None = None,
+        idCurso: int | None = None
+    ):
+    if idCampus is not None:
+        valorTotal = dbCalcularValorTotalInadimplente(idCampus=idCampus)
+        qtdeAlunosInad = dbContarAlunosInadimplentes(idCampus=idCampus)
+        
+    elif idCurso is not None: 
+        valorTotal = dbCalcularValorTotalInadimplente(idCurso=idCurso)
+        qtdeAlunosInad = dbContarAlunosInadimplentes(idCurso=idCurso)
+
+    else:
+        valorTotal = dbCalcularValorTotalInadimplente()
+        qtdeAlunosInad = dbContarAlunosInadimplentes()
+
+    valorTotal = valorTotal or 0
+    qtdeAlunosInad = qtdeAlunosInad or 0
+
+    if not qtdeAlunosInad:
+        return 0
+
+    return (valorTotal / qtdeAlunosInad)
+
+def dbContarBolsistas(
+        idCampus: int | None = None,
+        idCurso: int | None = None
+    ):
+    with SessionLocal() as session:
+        query = (
+            select(func.count(Aluno.pessoa_id.distinct()))
+            .join(Aluno.bolsas)
+            .where(Bolsa.status == StatusBolsa.ATIVA)
+        )
+        
+        if idCampus is not None:
+            query = query.where(Aluno.campus_id == idCampus)
+    
+        elif idCurso is not None:
+            query = query.where(Aluno.curso_id == idCurso)
+                
+        qtdeBolsistas = session.scalar(query)
+        
+        return qtdeBolsistas
+
+def dbCalcularTaxaBolsistas(
+        idCampus: int | None = None,
+        idCurso: int | None = None
+    ):
+    if idCampus is not None:
+        qtdeAlunos = dbContarAlunosAtivos(idCampus=idCampus)
+        qtdeBolsistas = dbContarBolsistas(idCampus=idCampus)
+
+    elif idCurso is not None: 
+        qtdeAlunos = dbContarAlunosAtivos(idCurso=idCurso)
+        qtdeBolsistas = dbContarBolsistas(idCurso=idCurso)
+            
+    else:
+        qtdeAlunos = dbContarAlunosAtivos()
+        qtdeBolsistas = dbContarBolsistas()
+
+    qtdeBolsistas = qtdeBolsistas or 0
+    qtdeAlunos = qtdeAlunos or 0
+
+    if not qtdeAlunos:
+        return 0
+
+    return (qtdeBolsistas / qtdeAlunos)
+
+def dbCalcularValorConcedidoPorBolsas(
+        idCampus: int | None = None,
+        idCurso: int | None = None
+    ):
+    with SessionLocal() as session:
+        query = (
+            select(func.sum(Curso.mensalidade_base * Bolsa.percentual_desconto))
+            .join(Bolsa.aluno)
+            .join(Aluno.curso)
+            .where(
+                Bolsa.status == StatusBolsa.ATIVA,
+                Aluno.status == StatusAluno.ATIVO
+            )
+        )
+        
+        if idCampus is not None:
+            query = query.where(Aluno.campus_id == idCampus)
+    
+        elif idCurso is not None:
+            query = query.where(Aluno.curso_id == idCurso)
+                
+        totalConcedido = session.scalar(query)
+        
+        return totalConcedido
+
+#  _____                                  _                       _ 
+# |  _  |                                (_)                     | |
+# | | | | _ __    ___  _ __   __ _   ___  _   ___   _ __    __ _ | |
+# | | | || '_ \  / _ \| '__| / _` | / __|| | / _ \ | '_ \  / _` || |
+# \ \_/ /| |_) ||  __/| |   | (_| || (__ | || (_) || | | || (_| || |
+#  \___/ | .__/  \___||_|    \__,_| \___||_| \___/ |_| |_| \__,_||_|
+#        | |                                                        
+#        |_|                                                        
---
+++ modulos/dashboard/dashboard_service.py
@@ -0,0 +1,219 @@
+from modulos.dashboard.dashboard_db import *
+
+
+#  _____                     _ 
+# |  __ \                   | |
+# | |  \/  ___  _ __   __ _ | |
+# | | __  / _ \| '__| / _` || |
+# | |_\ \|  __/| |   | (_| || |
+#  \____/ \___||_|    \__,_||_|
+
+# Retorna a quantidade de alunos ativos (em situação regular)
+def alunosAtivosTotal():
+    return dbContarAlunosAtivos()
+
+def alunosAtivosPorCampus(idCampus: int):
+    return dbContarAlunosAtivos(idCampus=idCampus)
+
+def alunosAtivosPorCurso(idCurso: int):
+    return dbContarAlunosAtivos(idCurso=idCurso)
+
+# Retorna a quantidade de alunos formados
+def alunosFormadosTotal():
+    return dbContarAlunosFormados()
+
+def alunosFormadosPorCampus(idCampus: int):
+    return dbContarAlunosFormados(idCampus=idCampus)
+
+def alunosFormadosPorCurso(idCurso: int):
+    return dbContarAlunosFormados(idCurso=idCurso)
+
+# Retorna a quantidade de alunos evadidos
+def alunosEvadidosTotal():
+    return dbContarAlunosEvadidos()
+
+def alunosEvadidosPorCampus(idCampus: int):
+    return dbContarAlunosEvadidos(idCampus=idCampus)
+
+def alunosEvadidosPorCurso(idCurso: int):
+    return dbContarAlunosEvadidos(idCurso=idCurso)
+
+# Calcula a taxa de evasão geral
+def taxaEvasaoGeral(): # Saída em porcentagem
+    return dbCalcularTaxaEvasao() * 100
+
+# Retorna a quantidade de professores
+def professoresTotal():
+    return dbContarProfessores()
+
+def professoresPorCampus(idCampus: int):
+    return dbContarProfessores(idCampus=idCampus)
+
+def professoresPorCurso(idCurso: int):
+    return dbContarProfessores(idCurso=idCurso)
+
+# Retorna a quantidade de cursos
+def cursosTotal():
+    return dbContarCursos()
+
+def cursosPorCampus(idCampus: int):
+    return dbContarCursos(idCampus)
+
+
+#   ___                    _                   _              
+#  / _ \                  | |                 (_)             
+# / /_\ \  ___   __ _   __| |  ___  _ __ ___   _   ___   ___  
+# |  _  | / __| / _` | / _` | / _ \| '_ ` _ \ | | / __| / _ \ 
+# | | | || (__ | (_| || (_| ||  __/| | | | | || || (__ | (_) |
+# \_| |_/ \___| \__,_| \__,_| \___||_| |_| |_||_| \___| \___/ 
+
+# Calcula o coeficiente de rendimento médio
+def crMedioTotal():
+    return dbCalcularCoefRendMedio()
+
+def crMedioPorCampus(idCampus: int):
+    return dbCalcularCoefRendMedio(idCampus=idCampus)
+
+def crMedioCurso(idCurso: int):
+    return dbCalcularCoefRendMedio(idCurso=idCurso)
+
+# Retorna a quantidade de alunos em categorias de desempenho 
+# (Excelente, Bom, Regular e Ruim)
+def agruparAlunosDesempenhoGeral():
+    return dbAgruparAlunosDesempenho()
+
+def agruparAlunosDesempenhoPorCampus(idCampus: int):
+    return dbAgruparAlunosDesempenho(idCampus=idCampus)
+
+def agruparAlunosDesempenhoPorCurso(idCurso: int):
+    return dbAgruparAlunosDesempenho(idCurso=idCurso)
+
+# Retorna dados dos alunos com baixo desempenho academico
+# (Baixo desempenho: CR < 5.5 ou 3+ reprovaçoes)
+def listarAlunosBaixoDesempenhoGeral():
+    return dbAlunosBaixoDesempenho()
+
+def listarAlunosBaixoDesempenhoPorCampus(idCampus: int):
+    return dbAlunosBaixoDesempenho(idCampus=idCampus)
+
+def listarAlunosBaixoDesempenhoPorCurso(idCurso: int):
+    return dbAlunosBaixoDesempenho(idCurso=idCurso)
+
+
+# ______  _                                   _              
+# |  ___|(_)                                 (_)             
+# | |_    _  _ __    __ _  _ __    ___   ___  _  _ __   ___  
+# |  _|  | || '_ \  / _` || '_ \  / __| / _ \| || '__| / _ \ 
+# | |    | || | | || (_| || | | || (__ |  __/| || |   | (_) |
+# \_|    |_||_| |_| \__,_||_| |_| \___| \___||_||_|    \___/ 
+
+# Calcula o valor recebido total
+def calcularReceitaTotal():
+    return dbCalcularReceita()
+
+def calcularReceitaPorCampus(idCampus: int):
+    return dbCalcularReceita(idCampus=idCampus)
+
+def calcularReceitaPorCurso(idCurso: int):
+    return dbCalcularReceita(idCurso=idCurso)
+
+# Calcula o valor a receber total
+def cacularTotalAReceberGeral():
+    return dbCalcularTotalAReceber()
+
+def cacularTotalAReceberPorCampus(idCampus: int):
+    return dbCalcularTotalAReceber(idCampus=idCampus)
+
+def cacularTotalAReceberPorCurso(idCurso: int):
+    return dbCalcularTotalAReceber(idCurso=idCurso)
+
+# Retorna a quantidade de alunos inadimplentes
+def alunosInadimplentesTotal():
+    return dbContarAlunosInadimplentes()
+
+def alunosInadimplentesPorCampus(idCampus: int):
+    return dbContarAlunosInadimplentes(idCampus=idCampus)
+
+def alunosInadimplentesPorCurso(idCurso: int):
+    return dbContarAlunosInadimplentes(idCurso=idCurso)
+
+# Calcula a taxa de inadimplencia (saída em porcentagem)
+def taxaInadimplenciaGeral():
+    return dbCalcularTaxaInadimplencia() * 100
+
+def taxaInadimplenciaPorCampus(idCampus: int):
+    return dbCalcularTaxaInadimplencia(idCampus=idCampus) * 100
+
+def taxaInadimplenciaPorCurso(idCurso: int):
+    return dbCalcularTaxaInadimplencia(idCurso=idCurso) * 100
+
+# Calcula o valor monetário não pago por inadimplencia
+def valorTotalInadimplente():
+    return dbCalcularValorTotalInadimplente()
+
+def valorTotalInadimplentePorCampus(idCampus: int):
+    return dbCalcularValorTotalInadimplente(idCampus=idCampus)
+
+def valorTotalInadimplentePorCurso(idCurso: int):
+    return dbCalcularValorTotalInadimplente(idCurso=idCurso)
+
+# Retorna a quantidade de mensalidades vencidas
+def mensalidadesVencidasTotal():
+    return dbContarMensalidadesVencidas()
+
+def mensalidadesVencidasPorCampus(idCampus: int):
+    return dbContarMensalidadesVencidas(idCampus=idCampus)
+
+def mensalidadesVencidasPorCurso(idCurso: int):
+    return dbContarMensalidadesVencidas(idCurso=idCurso)
+
+# Calcula a divida média (inadimplente) por aluno
+def dividaMediaTotal():
+    return dbCalcularDividaMedia()
+
+def dividaMediaPorCampus(idCampus: int):
+    return dbCalcularDividaMedia(idCampus=idCampus)
+
+def dividaMediaPorCurso(idCurso: int):
+    return dbCalcularDividaMedia(idCurso=idCurso)
+
+# Retorna a quantidade de alunos com bolsa ativa
+def alunosBolsistasTotal():
+    return dbContarBolsistas()
+
+def alunosBolsistasPorCampus(idCampus: int):
+    return dbContarBolsistas(idCampus=idCampus)
+
+def alunosBolsistasPorCurso(idCurso: int):
+    return dbContarBolsistas(idCurso=idCurso)
+
+# Calcula o percentual de bolsistas com bolsa ativa em relação aos alunos ativos
+def taxaBolsistaGeral():
+    return dbCalcularTaxaBolsistas()
+
+def taxaBolsistaPorCampus(idCampus: int):
+    return dbCalcularTaxaBolsistas(idCampus=idCampus)
+
+def taxaBolsistaPorCurso(idCurso: int):
+    return dbCalcularTaxaBolsistas(idCurso=idCurso)
+
+# Calcula o valor monetário total "perdido" por causa de bolsas
+def valorConcedidoPorBolsaTotal():
+    return dbCalcularValorConcedidoPorBolsas()
+
+def valorConcedidoPorBolsaPorCampus(idCampus: int):
+    return dbCalcularValorConcedidoPorBolsas(idCampus=idCampus)
+
+def valorConcedidoPorBolsaPorCurso(idCurso: int):
+    return dbCalcularValorConcedidoPorBolsas(idCurso=idCurso)
+
+
+#  _____                                  _                       _ 
+# |  _  |                                (_)                     | |
+# | | | | _ __    ___  _ __   __ _   ___  _   ___   _ __    __ _ | |
+# | | | || '_ \  / _ \| '__| / _` | / __|| | / _ \ | '_ \  / _` || |
+# \ \_/ /| |_) ||  __/| |   | (_| || (__ | || (_) || | | || (_| || |
+#  \___/ | .__/  \___||_|    \__,_| \___||_| \___/ |_| |_| \__,_||_|
+#        | |                                                        
+#        |_|                                                        
+
